992c126ca3aadbba34c3bd6e34286dea9bf7e310
Song ki ho <44698603+GodVas@users.noreply.github.com>
2021-04-19 20:33:49 +0900

    init commit

---
+++ src/skh6075/customshield/event/PlayerUseShieldEvent.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace skh6075\customshield\event;
+
+use pocketmine\entity\Entity;
+use pocketmine\event\Cancellable;
+use pocketmine\event\CancellableTrait;
+use pocketmine\event\Event;
+use pocketmine\player\Player;
+
+class PlayerUseShieldEvent extends Event implements Cancellable{
+    use CancellableTrait;
+
+    private Player $player;
+
+    private Entity $attacker;
+
+    private int $amount;
+
+    public function __construct(Player $player, Entity $attacker, int $amount) {
+        $this->player = $player;
+        $this->attacker = $attacker;
+        $this->amount = $amount;
+    }
+
+    final public function getPlayer(): Player{
+        return $this->player;
+    }
+
+    final public function getAttacker(): Entity{
+        return $this->attacker;
+    }
+
+    final public function getAmount(): int{
+        return $this->amount;
+    }
+
+    /**
+     * @description It supports the method you need to override the event.
+     *
+     * @param int $amount
+     */
+    public function setAmount(int $amount): void{
+        $this->amount = $amount;
+    }
+}
